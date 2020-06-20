0f9193acbb51628a6c006bb68c832cccc95bc9e0
Luis Fernando Cezar dos Santos <luisfernando_cezar@hotmail.com>
2020-06-20 15:25:14 -0300

    OSLEK | implements LogIN/Logout User;

--- appOslek/src/services/Usuario.service.ts
+++ appOslek/src/services/Usuario.service.ts
@@ -25,7 +25,7 @@ export class UsuarioService implements IUsuarioService {
     authUsuario(usuario: Usuario): Observable<Object> {
         if (!usuario.email) throw new Error("O campo email é obrigatório.");
         if (!usuario.senha) throw new Error("O campo senha é obrigatório.");
-        
+        this.logar(usuario);
         return this.http.post(this.apiURL +"/buscarusuario/", usuario);
     }
     buscarUsuario(id: number): Usuario {
@@ -41,8 +41,11 @@ export class UsuarioService implements IUsuarioService {
         let usuarioLogado: Usuario = JSON.parse(localStorage.getItem("usuarioLogado"));
         return usuarioLogado;
     }
+    logout(): void {
+        localStorage.removeItem("usuarioLogado");
+    }
     removerUsuario(usuario: Usuario): Observable<Object> {
         return this.http.delete(this.apiURL + "/" + usuario.id);
     }
-
+  
 }
