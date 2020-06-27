704e02dec63b0798c387aa218b929f3abd069d0b
Luis Fernando Cezar dos Santos <luisfernando_cezar@hotmail.com>
2020-06-27 15:22:14 -0300

    OSLEK | implments valid CPF

--- appOslek/src/pages/login/login.ts
+++ appOslek/src/pages/login/login.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
 import { ChamadoClientePage } from '../chamado-cliente/chamado-cliente';
 import { Usuario } from '../../models/Usuario';
@@ -7,6 +7,7 @@ import { ChamadoFuncionarioPage } from '../chamado-funcionario/chamado-funcionar
 import { ChamadoService } from '../../services/Chamado.service';
 
 import { trigger, style, animate, transition } from '@angular/animations';
+import { ValidarCpf } from './ValidarCpf';
 
 @IonicPage()
 @Component({
@@ -16,37 +17,37 @@ import { trigger, style, animate, transition } from '@angular/animations';
   animations: [
     trigger(
       'login', [
-        transition(':enter', [
-          style({
-            opacity: 0
-          }),
-          animate("1s ease-in-out", style({
-            opacity: 1
-          }))
-        ]),
-        transition(':leave', [
-          style({
-            opacity: 0
-          })
-        ])
-      ],
+      transition(':enter', [
+        style({
+          opacity: 0
+        }),
+        animate("1s ease-in-out", style({
+          opacity: 1
+        }))
+      ]),
+      transition(':leave', [
+        style({
+          opacity: 0
+        })
+      ])
+    ],
     ),
     trigger(
       'registro', [
-        transition(':enter', [
-          style({
-            opacity: 0
-          }),
-          animate("1s ease-in-out", style({
-            opacity: 1
-          }))
-        ]),
-        transition(':leave', [
-          style({
-            opacity: 0
-          })
-        ])
-      ],
+      transition(':enter', [
+        style({
+          opacity: 0
+        }),
+        animate("1s ease-in-out", style({
+          opacity: 1
+        }))
+      ]),
+      transition(':leave', [
+        style({
+          opacity: 0
+        })
+      ])
+    ],
     ),
   ]
 
@@ -60,48 +61,60 @@ export class LoginPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public usuarioServices: UsuarioService, public chamadoServices: ChamadoService) {
-      localStorage.clear();
+    localStorage.clear();
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
   }
 
-  
-  goOpenSystem(){
+  goOpenSystem() {
     this.usuarioServices.login(this.usuario).subscribe((success) => {
-      if(success[0].tipo == "Cliente"){  
+      if (success[0].tipo == "Cliente") {
         console.log(success[0].tipo);
         this.usuarioServices.logar(success);
         this.navCtrl.setRoot(ChamadoClientePage);
 
-      }else if(success[0].tipo == "Funcionário"){
+      } else if (success[0].tipo == "Funcionário") {
         console.log(success[0].tipo);
         this.usuarioServices.logar(success);
         this.navCtrl.setRoot(ChamadoFuncionarioPage);
       }
-      
+
     }, (error) => {
       console.log(error);
     });
+  }
+
+  validCpf(usuario: Usuario): boolean {
+    var cpfLimpo = usuario.cpf.replace(".", '').replace(".", '').replace("-", '');
+    if (ValidarCpf.cpf(cpfLimpo)) {
+      return true;
+    }
+    return false;
   }
 
   cadastrarUsuario() {
-    this.usuarioServices.criarUsuario(this.usuario).subscribe((success) => {
-      console.log(success);
-      this.navCtrl.setRoot(LoginPage);
-    }, (error) => {
-      console.log(error);
-    });
+    if (this.validCpf(this.usuario)) {
+      this.usuarioServices.criarUsuario(this.usuario).subscribe((success) => {
+        console.log(success);
+        this.navCtrl.setRoot(LoginPage);
+      }, (error) => {
+        console.log(error);
+      });
+    }else{
+      console.log("CPF inválido!")
+    }
   }
 
-  exibirRegistrar(){
+  exibirRegistrar() {
     this.login = false;
     this.cadastro = true;
   }
 
-  exibirLogin(){
+  exibirLogin() {
     this.login = true;
     this.cadastro = false;
   }
+
 }
